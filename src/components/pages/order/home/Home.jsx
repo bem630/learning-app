e30f86c34f977ff271fa47ac7d715a36b46d7062
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-06 22:35:53 +0100

    ✨  F02: ORDER> HOME > Add <Services/> and <FAQSection/>

--- src/components/pages/order/home/Home.jsx
+++ src/components/pages/order/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import CallToAction from './CallToAction'
+import FAQSection from './FAQSection'
+import Services from './Services'
 
 export default function Home() {
   return (
     <div>
         <Banner/>
+        <Services/>
         <CallToAction/>
+        <FAQSection/>
     </div>
   )
 }
