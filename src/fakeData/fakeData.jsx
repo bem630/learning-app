f95910b1330fd8c89d561589262af6f933559af7
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-06 21:31:51 +0100

    ✨ F01: Add Data

---
+++ src/fakeData/fakeData.jsx
@@ -0,0 +1,87 @@
+// navLinksConfig.js
+export const navLinks = [
+    { 
+        id: "1", 
+        path: "/", 
+        label: "Accueil" 
+    },
+    { 
+        id: "2", 
+        path: "/apropos", 
+        label: "Apropos" 
+    },
+    { 
+        id: "3", 
+        path: "/formations", 
+        label: "Formations" 
+    },
+    { 
+        id: "4", 
+        path: "/evenements", 
+        label: "Evenements" 
+    },
+    { 
+        id: "5", 
+        path: "/blogs", 
+        label: "Blogs" 
+    },
+    { 
+        id: "6", 
+        path: "/contact", 
+        label: "Contact" 
+    },
+];
+
+export const testimonialsData = [
+    {
+        id: "1",
+        name: 'Aissata Diallo',
+        course: 'Développeuse Web',
+        feedback: 'Les formations sont excellentes et m’ont aidée à évoluer dans ma carrière. Je recommande vivement !',
+        rating: 5,
+        image: '/src/assets/teacher-1.jpg'
+    },
+    {
+        id: "2",
+        name: 'Mamoudou koulibaly',
+        course: 'Designer Graphique',
+        feedback: 'Une expérience très enrichissante. Les formateurs sont très professionnels et les cours sont bien structurés.',
+        rating: 4,
+        image: '/src/assets/teacher-2.jpg'
+    },
+    {
+        id: "3",
+        name: 'Lamine Conde',
+        course: 'Chef de Projet',
+        feedback: 'J’ai appris énormément de choses et j’ai pu mettre en pratique tout ce que j’ai appris dans des projets concrets.',
+        rating: 5,
+        image: '/src/assets/teacher-3.jpg'
+    },
+];
+
+export const partnerLogos = [
+    {
+        id: "1",
+        url: '/src/assets/orange.jpg',
+    },
+    {
+        id: "2",
+        url: '/src/assets/mtn.png',
+    },
+    {
+        id: "3",
+        url: '/src/assets/nsia.png',
+    },
+    {
+        id: "4",
+        url: '/src/assets/vista.jpeg',
+    },
+    {
+        id: "5",
+        url: '/src/assets/bcrg.png',
+    },
+    {
+        id: "6",
+        url: '/src/assets/Banque_Islamique_de_Guinée.webp.png',
+    },
+];
