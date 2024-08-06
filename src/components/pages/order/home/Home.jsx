2c997eeb8d1c2477a5176e553eccc7771a7b53d9
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-06 22:04:46 +0100

    ✨  F02: ORDER> HOME > Add <CallToAction/>

--- src/components/pages/order/home/Home.jsx
+++ src/components/pages/order/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './Banner'
+import CallToAction from './CallToAction'
 
 export default function Home() {
   return (
     <div>
         <Banner/>
+        <CallToAction/>
     </div>
   )
 }
