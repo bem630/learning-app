5773b36b426aa1ddc1bf551ee865276700fe7012
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-13 23:51:52 +0100

    F04: ORDER > HOME > Add Layout <FeaturedCourses/> and <UpcomingEvents/>

--- src/components/pages/order/home/Home.jsx
+++ src/components/pages/order/home/Home.jsx
@@ -3,12 +3,16 @@ import Banner from './Banner'
 import CallToAction from './CallToAction'
 import FAQSection from './FAQSection'
 import Services from './Services'
+import FeaturedCourses from './FeaturedCourses'
+import UpcomingEvents from './UpcomingEvents'
 
 export default function Home() {
   return (
     <div>
         <Banner/>
         <Services/>
+        <FeaturedCourses/>
+        <UpcomingEvents/>
         <CallToAction/>
         <FAQSection/>
     </div>
